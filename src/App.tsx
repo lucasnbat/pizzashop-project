4f95a050f0df6722c3fcf2dacdd42605acce45ee
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-19 13:37:34 -0400

    feat: adiciona componentes Dashboard e SignIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <Button>Crespos kkkk</Button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,3 @@
+export function Dashboard() {
+  return <h1>Ai qui diliça</h1>
+}
