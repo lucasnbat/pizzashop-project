d1a73e9169836085bf5064250ae1fd5221712d5d
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-28 08:21:07 -0400

    feat: adicionando células e regras à tabela

--- src/pages/app/punches/punches.tsx
+++ src/pages/app/punches/punches.tsx
@@ -1,6 +1,15 @@
 import { Helmet } from "react-helmet-async";
 import { Input } from "../../../components/ui/input";
-import { Table, TableHead, TableHeader, TableRow } from "../../../components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../../components/ui/table";
+import { Button } from "../../../components/ui/button";
+import { Edit, Search, X } from "lucide-react";
 
 export function Punches() {
   return (
@@ -19,17 +28,70 @@ export function Punches() {
         <div className="rounded-medium border">
           <Table>
             <TableHeader>
-                <TableRow>
-                    <TableHead></TableHead>
-                    <TableHead>Identificador</TableHead>
-                    <TableHead>Genérico</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Funcionário</TableHead>
-                    <TableHead>Numeral</TableHead>
-                    <TableHead></TableHead>
-                    <TableHead></TableHead>
-                </TableRow>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead>Data</TableHead>
+                <TableHead>Dia da semana</TableHead>
+                <TableHead>Entrada</TableHead>
+                <TableHead>Saída</TableHead>
+                <TableHead>Entrada</TableHead>
+                <TableHead>Saída</TableHead>
+                <TableHead>Entrada</TableHead>
+                <TableHead>Saída</TableHead>
+                <TableHead>Normais</TableHead>
+                <TableHead>Falta</TableHead>
+                <TableHead>Atraso</TableHead>
+                <TableHead>Ex50%</TableHead>
+                <TableHead>Ex100%</TableHead>
+                <TableHead>EN50%</TableHead>
+                <TableHead>ExNot</TableHead>
+                <TableHead>Not.</TableHead>
+                <TableHead>Not. Tot.</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
             </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Button variant="outline" size="sm">
+                    <Search className="h-3 w-3" />
+                    <span className="sr-only">Detalhes da batida</span>
+                  </Button>
+                </TableCell>
+                <TableCell>24/07/2024</TableCell>
+                <TableCell>TER</TableCell>
+                <TableCell>07:12</TableCell>
+                <TableCell>11:30</TableCell>
+                <TableCell>13:00</TableCell>
+                <TableCell>17:00</TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>08:18</TableCell>
+                <TableCell>00:20</TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>
+                  <div className="flex items-center gap-2">
+                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">
+                      Pendente
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <Button variant="ghost" size="sm">
+                    <Edit className="mr-2 h-3 w-3" />
+                    Justificar
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
           </Table>
         </div>
       </div>
