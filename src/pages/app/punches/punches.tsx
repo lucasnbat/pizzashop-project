dc6712e9e07552d44463248b300176425ed2ddcf
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-28 09:42:32 -0400

    feat: adiciona novas linhas a tabela

--- src/pages/app/punches/punches.tsx
+++ src/pages/app/punches/punches.tsx
@@ -9,7 +9,7 @@ import {
   TableRow,
 } from "../../../components/ui/table";
 import { Button } from "../../../components/ui/button";
-import { Edit, Search, X } from "lucide-react";
+import { Edit, Search } from "lucide-react";
 
 export function Punches() {
   return (
@@ -52,45 +52,49 @@ export function Punches() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Button variant="outline" size="sm">
-                    <Search className="h-3 w-3" />
-                    <span className="sr-only">Detalhes da batida</span>
-                  </Button>
-                </TableCell>
-                <TableCell>24/07/2024</TableCell>
-                <TableCell>TER</TableCell>
-                <TableCell>07:12</TableCell>
-                <TableCell>11:30</TableCell>
-                <TableCell>13:00</TableCell>
-                <TableCell>17:00</TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell>08:18</TableCell>
-                <TableCell>00:20</TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">
-                      Pendente
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <Button variant="ghost" size="sm">
-                    <Edit className="mr-2 h-3 w-3" />
-                    Justificar
-                  </Button>
-                </TableCell>
-              </TableRow>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <Button variant="outline" size="sm">
+                        <Search className="h-3 w-3" />
+                        <span className="sr-only">Detalhes da batida</span>
+                      </Button>
+                    </TableCell>
+                    <TableCell>24/07/2024</TableCell>
+                    <TableCell>TER</TableCell>
+                    <TableCell>07:12</TableCell>
+                    <TableCell>11:30</TableCell>
+                    <TableCell>13:00</TableCell>
+                    <TableCell>17:00</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>08:18</TableCell>
+                    <TableCell>00:20</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <span className="h-2 w-2 rounded-full bg-slate-400" />
+                        <span className="font-medium text-muted-foreground">
+                          Pendente
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <Button variant="ghost" size="sm">
+                        <Edit className="mr-2 h-3 w-3" />
+                        Justificar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
             </TableBody>
           </Table>
         </div>
