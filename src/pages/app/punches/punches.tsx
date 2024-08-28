f255342b62bc4913502160bac18a0c134939f608
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-28 09:47:06 -0400

    feat: adiciona componente PunchTableRow

--- src/pages/app/punches/punches.tsx
+++ src/pages/app/punches/punches.tsx
@@ -3,13 +3,11 @@ import { Input } from "../../../components/ui/input";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "../../../components/ui/table";
-import { Button } from "../../../components/ui/button";
-import { Edit, Search } from "lucide-react";
+import PunchTableRow from "./punch-table-row";
 
 export function Punches() {
   return (
@@ -53,47 +51,7 @@ export function Punches() {
             </TableHeader>
             <TableBody>
               {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell>
-                      <Button variant="outline" size="sm">
-                        <Search className="h-3 w-3" />
-                        <span className="sr-only">Detalhes da batida</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell>24/07/2024</TableCell>
-                    <TableCell>TER</TableCell>
-                    <TableCell>07:12</TableCell>
-                    <TableCell>11:30</TableCell>
-                    <TableCell>13:00</TableCell>
-                    <TableCell>17:00</TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>08:18</TableCell>
-                    <TableCell>00:20</TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" size="sm">
-                        <Edit className="mr-2 h-3 w-3" />
-                        Justificar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                );
+                return <PunchTableRow key={i} />;
               })}
             </TableBody>
           </Table>
