e40319eae0de5764ce4c451728c8f963f59604a6
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-30 13:18:48 -0400

    feat: adiicona delay no axios + skeletons para loading state

--- notes.md
+++ notes.md
@@ -7,3 +7,4 @@
   - Havendo chamada de uma requisição por um outro componenete,
     se ela já foi feita, ele usa o cache
 - crie o arquivo react-query instanciando o client (/lib/react-query.ts)
+- `pnpm dlx shadcn-ui@latest add skeleton`: para efeitos de carregamento
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,14 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // cookies do front automat. enviados para o back
 })
+
+// Se houver delay habilitado, vai interceptar cada requisição e
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    // botar tempo de delay
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    // retorna a req com as config noramis
+    return config
+  })
+}
