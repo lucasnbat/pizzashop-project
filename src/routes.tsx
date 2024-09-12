1c79e0b004ea8d77b5915b30ba16d1e502515567
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-12 09:28:37 -0400

    adiciona pagina de error

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,41 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import { AppLayout } from './pages/_layouts/app'
-import { AuthLayout } from './pages/_layouts/auth'
-import NotFound from './pages/404'
-import { Dashboard } from './pages/app/dashboard/dashboard'
-import { Orders } from './pages/app/orders/orders'
-import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sign-up'
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import NotFound from "./pages/404";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
+import { Orders } from "./pages/app/orders/orders";
+import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
+import { Error } from "./pages/error";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Dashboard />,
       },
       {
-        path: '/orders',
+        path: "/orders",
         element: <Orders />,
       },
     ],
   },
   {
-    path: '/',
+    path: "/",
     element: <AuthLayout />,
     children: [
       {
-        path: '/sign-in',
+        path: "/sign-in",
         element: <SignIn />,
       },
       {
-        path: '/sign-up',
+        path: "/sign-up",
         element: <SignUp />,
       },
     ],
   },
-])
+  {
+    path: "*", // se entrar em qualquer outro endereço, manda para...
+    element: <NotFound />, // página notfound
+  },
+]);
