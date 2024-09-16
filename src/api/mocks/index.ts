46273a681b93f9424045f5ecf39bb7b7a1cf5377
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-16 11:20:52 -0400

    adiciona arquivo de mock do register restaurant

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,11 +1,12 @@
 import { env } from '@/env'
 import { setupWorker } from 'msw/browser'
 import { signInMock } from './sign-in-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(signInMock, registerRestaurantMock)
 
 export async function enableMSW() {
-    if (env.MODE !== 'test'){ // se não for ambiente de test, não rodará no msw
+    if (env.MODE !== 'test') { // se não for ambiente de test, não rodará no msw
         return
     }
 
