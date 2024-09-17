9318ba61c5ec5c9d6b2caa6f874644907025ee9c
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-17 11:00:44 -0400

    adiciona primeiro teste de att. de perfil

--- test/store-profile.e2e-spec.ts
+++ test/store-profile.e2e-spec.ts
@@ -1,45 +1,25 @@
 import { expect, test } from '@playwright/test'
 
-test('sign up successfully', async ({ page }) => {
-    // navega para sign-up e espera tudo de req carregar o conteudo da pag
-    await page.goto('/sign-up', { waitUntil: 'networkidle' })
+test('update profile successfully', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
 
-    await page.getByLabel('Nome do estabelecimento').fill('Pizza shop')
-    await page.getByLabel('Seu nome').fill('John Doe')
-    await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
-    await page.getByLabel('Seu celular').fill('66988775599')
+    // abre menu
+    await page.getByRole('button', { name: 'Pizza shop' }).click()
+    await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
 
-    page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+    // inseri info
+    await page.getByLabel('Nome').fill('Rocket Pizza')
+    await page.getByLabel('Descrição').fill('Tchain')
 
-    const toast = page.getByText('Restaurante cadastrado com sucesso')
+    // salva
+    await page.getByRole('button', { name: 'Salvar' }).click()
+
+    // aguarda quaisquer reqs http sendo feitas
+    await page.waitForLoadState('networkidle')
+
+    const toast = page.getByText('Perfil atualizado com sucesso!')
 
     expect(toast).toBeVisible()
 
-    // delay para prolongar o teste para aparecer o final (bug do playwritght)
     await page.waitForTimeout(2000)
-})
-
-test('sign up with error', async ({ page }) => {
-    // navega para sign-up e espera tudo de req carregar o conteudo da pag
-    await page.goto('/sign-up', { waitUntil: 'networkidle' })
-
-    await page.getByLabel('Nome do estabelecimento').fill('Tchain shop')
-    await page.getByLabel('Seu nome').fill('John Doe')
-    await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
-    await page.getByLabel('Seu celular').fill('66988775599')
-
-    page.getByRole('button', { name: 'Finalizar cadastro' }).click()
-
-    const toast = page.getByText('Erro ao cadastrar restaurante')
-    expect(toast).toBeVisible()
-    // delay para prolongar o teste para aparecer o final (bug do playwritght)
-    // await page.waitForTimeout(2000)
-})
-
-test('navigate to login page', async ({ page }) => {
-    await page.goto('/sign-up', { waitUntil: 'networkidle' })
-
-    await page.getByRole('link', { name: 'Fazer login' }).click()
-
-    expect(page.url()).toContain('/sign-in')
 })
